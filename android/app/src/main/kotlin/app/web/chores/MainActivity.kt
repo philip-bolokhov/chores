05a59a5ca9b208614e9f25dd911538a6a56480ef
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2020-07-19 22:16:04 -0700

    Move 'MainActivity.kt' into the properly name package folder

---
+++ android/app/src/main/kotlin/app/web/chores/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.web.chores
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/example/chores/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package app.web.daily_chore
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}

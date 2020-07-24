60eed510c1ed184965d08a37e8c0fbe3ae59c9b9
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2020-07-24 08:50:43 -0700

    Limit the width of the home page on wide screens

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const MAX_PAGE_WIDTH = 800;
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -15,38 +17,41 @@ class MyApp extends StatelessWidget {
       title: 'Daily Chore',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Chores'),
+      home: LayoutBuilder(builder: (context, constraints) {
+        if (constraints.maxWidth > MAX_PAGE_WIDTH) {
+          return _wideLayout(constraints);
+        } else {
+          return _normalLayout();
+        }
+      }),
     );
+  }
+
+  Widget _wideLayout(BoxConstraints constraints) {
+    double padding = (constraints.maxWidth - MAX_PAGE_WIDTH) / 2.0;
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: padding),
+      child: MyHomePage(),
+    );
+  }
+
+  Widget _normalLayout() {
+    return MyHomePage();
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -75,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ]),
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
+          title: Text('Chores'),
           centerTitle: true,
         ),
         body: TabBarView(children: [

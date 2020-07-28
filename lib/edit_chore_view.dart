908d9dbf52e5325bec30bf12086b90fa7137b5ac
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2020-07-28 01:09:39 -0700

    Handle the back arrow event ourselves

--- lib/edit_chore_view.dart
+++ lib/edit_chore_view.dart
@@ -9,14 +9,22 @@ class EditChoreView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            print("navigating back");
+            Navigator.pop(context);
+          },
+        ),
         title: Text('Edit the chore'),
       ),
       body: Center(
-          child: Text(
-        "Editing '${chore['title']}'",
-        style: TextStyle(
-            fontSize: 16, color: Colors.purple, fontWeight: FontWeight.bold),
-      )),
+        child: Text(
+          "Editing '${chore['title']}'",
+          style: TextStyle(
+              fontSize: 16, color: Colors.purple, fontWeight: FontWeight.bold),
+        ),
+      ),
     );
   }
 }
